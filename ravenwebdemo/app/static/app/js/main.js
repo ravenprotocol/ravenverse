b99d5cadce920a15ec50b362b1d2286343cc7825
Nipun Bharti <nipun@matelabs.in>
2020-07-25 23:50:24 +0530

    Added: Basic frontend setup

--- ravenwebdemo/app/static/app/js/main.js
+++ ravenwebdemo/app/static/app/js/main.js
@@ -1,3 +1,38 @@
 $(document).ready(function () {
-    alert("Hello");
+	$("#linear").addClass('active');
+	setDisplayNone();
+	$("#linearContainer").css("display", "block");
+});
+
+function removeClasses() {
+	$("#core").removeClass('active');
+	$("#linear").removeClass('active');
+	$("#logistic").removeClass('active');
+}
+
+function setDisplayNone() {
+	$("#coreContainer").css("display", "none");
+	$("#linearContainer").css("display", "none");
+	$("#logisticContainer").css("display", "none");
+}
+
+$("#core").click(function () {
+	removeClasses();
+	setDisplayNone();
+	$("#core").addClass('active');
+	$("#coreContainer").css("display", "block");
+});
+
+$("#linear").click(function () {
+	removeClasses();
+	setDisplayNone();
+	$("#linear").addClass('active');
+	$("#linearContainer").css("display", "block");
+});
+
+$("#logistic").click(function () {
+	removeClasses();
+	setDisplayNone();
+	$("#logistic").addClass('active');
+	$("#logisticContainer").css("display", "block");
 });
