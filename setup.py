89195d317de9c85d5009badb2ddb4034755956fb
Kailash <ahikailash1@gmail.com>
2020-10-07 19:05:20 +0530

    Setup.py file added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Raven Distribution Framework',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'python-socketio==4.5.1',
+        'requests==2.23.0'
+    ],
+)
