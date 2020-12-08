86770504604c97d29a8b321850dd10087f8da352
pranshurastogi29 <43302812+pranshurastogi29@users.noreply.github.com>
2020-12-08 15:38:28 +0530

    Update loses.py
    added poisson , kl_div , huber, categorical_hinge , log cosh(not completed)

--- ravop/ml/loses.py
+++ ravop/ml/loses.py
@@ -58,3 +58,65 @@ def sparse_cross_entropy(y_true, y_pred, with_logit=True):
   N = y_pred.shape[0]
   loss = R.elemul(R.Scalar(-1),R.div(R.sum(R.natlog(y_pred[R.len(y_pred),y_true])),R.Scalar(N)))
   return loss
+
+
+def categorical_hinge(y_true, y_pred):
+
+  if not isinstance(y_true, R.Tensor):
+      y_true = R.Tensor(y_true)
+  if not isinstance(y_pred, R.Tensor):
+      y_pred = R.Tensor(y_pred)
+
+  neg = R.max(R.elemul(R.sub(R.Scalar(-1),y_true),y_pred))
+  pos = R.sum(R.elemul(y_true, y_pred))
+  loss = R.max((R.sub(neg, pos),R.Scalar(1)),R.Scalar(0))
+
+  return loss
+
+
+def huber(y_true, y_pred, d):
+
+  if not isinstance(y_true, R.Tensor):
+      y_true = R.Tensor(y_true)
+  if not isinstance(y_pred, R.Tensor):
+      y_pred = R.Tensor(y_pred)
+
+  d = R.Scalar(d)
+  x = R.sub(y_true, y_pred)
+
+  if R.abs(x) <= d:
+    return R.elemul(R.Scalar(d),R.elemul(x,x))
+
+  if R.abs(x) > d:
+    return R.add(R.elemul(R.Scalar(d),R.mul(d,d)) ,R.elemul(d,R.sub(R.abs(x),d)))
+
+def KL_div_loss(y_true, y_pred, d):
+
+  if not isinstance(y_true, R.Tensor):
+      y_true = R.Tensor(y_true)
+  if not isinstance(y_pred, R.Tensor):
+      y_pred = R.Tensor(y_pred)
+
+  y_pred = R.clip(y_pred, R.epsilon(), R.Saclar(1)-R.epsilon())
+
+  return R.elemul(y_true,R.natlog(R.div(y_true,y_pred)))
+
+def Poisson_loss(y_true, y_pred):
+  
+  if not isinstance(y_true, R.Tensor):
+      y_true = R.Tensor(y_true)
+  if not isinstance(y_pred, R.Tensor):
+      y_pred = R.Tensor(y_pred)
+
+  y_pred = R.clip(y_pred, R.epsilon(), R.Saclar(1)-R.epsilon())
+
+  return R.sub(y_pred,R.elemul(y_true, R.natlog(y_pred)))
+  
+def Logcosh(y_true, y_pred):
+  
+  """ not completed """
+  
+  if not isinstance(y_true, R.Tensor):
+      y_true = R.Tensor(y_true)
+  if not isinstance(y_pred, R.Tensor):
+      y_pred = R.Tensor(y_pred)
