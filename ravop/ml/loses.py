0efaafdd2f8fc8cb623d7a7b0431358ccb6e2563
pranshurastogi29 <43302812+pranshurastogi29@users.noreply.github.com>
2020-12-08 15:46:08 +0530

    Update loses.py
    added softmax , sigmoid

--- ravop/ml/loses.py
+++ ravop/ml/loses.py
@@ -1,5 +1,12 @@
 import ravop.core as R
 
+def softmax(x):
+    e_x = R.exp(R.sub(R.Scalar(1),R.max(x)))
+    return R.div(e_x ,R.sum(e_x))
+
+def sigmoid(x):
+  return R.div(R.Scalar(1),R.add(R.Scalar(1),R.exp(R.elemul(R.scalar(-1),x)))) 
+
 def mean_ab_error(y_true ,y_pred):
 
   if not isinstance(y_true, R.Tensor):
@@ -29,10 +36,13 @@ def root_mean_sq_error(y_true ,y_pred):
 
 
 def log_loss(y_true , y_pred , with_logit=True):
+  
   if with_logit:
-    y_pred = R.sigmoid(y_pred)
+    y_pred = sigmoid(y_pred)
+    
   else:
     pass
+  
   y_pred = R.clip(y_pred, R.epsilon(), R.sub(R.Scalar(1), R.epsilon()))
   loss = R.elemul(R.Scalar(-1) ,R.mean(R.elemul(y_true ,R.natlog(y_pred)) ,R.elemul((R.sub(R.Scalar(1), y_true)) ,R.natlog(R.sub(R.Scalar(1),y_pred)))))
 
@@ -40,23 +50,31 @@ def log_loss(y_true , y_pred , with_logit=True):
 
 
 def one_hot_cross_entropy(y_true, y_pred, with_logit=True):
+  
   if with_logit:
-    y_pred = R.softmax(y_pred)
+    y_pred = softmax(y_pred)
+    
   else:
     pass
+  
   y_pred = R.clip(y_pred, R.epsilon(), R.div(R.Scalar(1) ,R.epsilon()))
   N = y_pred.shape[0]
   loss = R.div(R.elemul(R.Scalar(-1),R.mul(R.sum(y_true ,R.natlog(R.add(y_pred,1e-9))))),R.Scalar(N))
+  
   return loss
 
 def sparse_cross_entropy(y_true, y_pred, with_logit=True):
+  
   if with_logit:
-    y_pred = R.softmax(y_pred)
+    y_pred = softmax(y_pred)
+    
   else:
     pass
+  
   y_pred = R.clip(y_pred, R.epsilon(), R.div(R.Scalar(1) ,R.epsilon()))
   N = y_pred.shape[0]
   loss = R.elemul(R.Scalar(-1),R.div(R.sum(R.natlog(y_pred[R.len(y_pred),y_true])),R.Scalar(N)))
+  
   return loss
 
 
