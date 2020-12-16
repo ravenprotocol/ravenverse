ffaf88c7027450d506787baad9b0a34e89ebb9e2
Kailash <ahikailash1@gmail.com>
2020-12-16 15:48:36 +0530

    Installation script added

---
+++ install.sh
@@ -0,0 +1,8 @@
+sudo apt-get install python3-pip
+sudo pip3 install virtualenv
+virtualenv venv -p python3
+venv/bin/pip3 install -r requirements.txt
+
+# Install redis
+sudo apt install redis-server
+
