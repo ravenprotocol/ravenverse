d23e63219795eaac09b3e19b12df2a43cb9f87d7
Kailash <ahikailash1@gmail.com>
2021-05-24 17:58:54 +0530

    Script setup.sh updated

--- setup.sh
+++ setup.sh
@@ -1,7 +1,10 @@
-pip install git+https://github.com/ravenprotocol/ravcom.git
-pip install git+https://github.com/ravenprotocol/ravop.git
-pip install git+https://github.com/ravenprotocol/ravml.git
-pip install git+https://github.com/ravenprotocol/ravsock.git
+pip install git+https://github.com/ravenprotocol/ravcom.git@0.1-alpha
+pip install git+https://github.com/ravenprotocol/ravop.git@0.1-alpha
+pip install git+https://github.com/ravenprotocol/ravml.git@0.1-alpha
+pip install git+https://github.com/ravenprotocol/ravsock.git@0.1-alpha
 pip install git+https://github.com/ravenprotocol/ravviz.git
 
-git clone https://github.com/ravenprotocol/ravjs.git
+wget https://github.com/ravenprotocol/ravjs/archive/refs/tags/0.1-alpha.zip
+unzip 0.1-alpha.zip
+rm 0.1-alpha.zip
+mv ravjs-0.1-alpha ravjs
