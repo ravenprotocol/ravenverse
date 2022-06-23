3a17e9d36002373fd1a279dca5f953cd826f8314
Unnikrishnan Menon <menon.uk1998@gmail.com>
2022-06-23 18:16:18 +0530

    Compile-Execute Functionality for ANN and CNN

--- ANN_compile.py
+++ ANN_compile.py
@@ -11,16 +11,17 @@ from sklearn.preprocessing import normalize
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 
-import ravop.ravop as R
+import ravop as R
 
 R.initialize(ravenverse_token='YOUR_TOKEN')
-algo = R.Graph(name='ann', algorithm='neural_network', approach='distributed')
+R.flush()
+R.Graph(name='ann', algorithm='neural_network', approach='distributed')
 
 data = datasets.load_iris()
 X = data.data
 y = data.target
-X=normalize(X,axis=0)
-X, X_test, y , y_test = train_test_split(X, y, test_size=0.33)
+X= normalize(X,axis=0)
+X, X_test, y , y_test = train_test_split(X, y, test_size=0.33, random_state=42)
 
 def to_categorical(x, n_col=None):
     if not n_col:
@@ -32,7 +33,6 @@ def to_categorical(x, n_col=None):
 y = to_categorical(y.astype("int"))
 n_samples, n_features = X.shape
 n_hidden = 15
-print("No of samples:",n_samples)
 
 optimizer = RMSprop()
 
@@ -48,14 +48,11 @@ model.summary()
 
 print('\nTraining...')
 
-train_err = model.fit(X, y, n_epochs=1, batch_size=25)
+model.fit(X, y, n_epochs=6, batch_size=25)
 
 print('\nTesting...')
 
-y_pred = np.argmax(model.predict(X_test)(),axis=1)
+prediction = model.predict(X_test)
+prediction.persist_op(name='test_prediction')
 
-accuracy = accuracy_score(y_test, y_pred)
-
-print ("Accuracy:", accuracy)
-
-algo.end()
+R.activate()
