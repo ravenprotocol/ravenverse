fd09db69236add26bc32d2546c45e28164141b77
Kailash <ahikailash1@gmail.com>
2022-08-18 11:58:44 +0530

    Client code updated

--- run_distributed_client.py
+++ run_distributed_client.py
@@ -8,5 +8,5 @@ from ravpy.distributed.participate import participate
 from ravpy.initialize import initialize
 
 if __name__ == '__main__':
-    client = initialize(ravenverse_token=os.environ.get("TOKEN"))
+    client = initialize(os.environ.get("TOKEN"))
     participate()
