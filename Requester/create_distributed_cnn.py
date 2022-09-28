f877267ed3a821a599087870454233f8a3a24ccc
Unnikrishnan Menon <menon.uk1998@gmail.com>
2022-09-28 14:03:07 +0530

    code restructured for onnx testing
    Co-authored-by: Anirudh <senseianirudh28@gmail.com>

--- Requester/create_distributed_cnn.py
+++ Requester/create_distributed_cnn.py
@@ -4,7 +4,7 @@ load_dotenv()
 import os
 import ravop as R
 
-from .examples import cnn
+from examples import cnn, onnx
 
 # Initialize and create graph
 R.initialize(ravenverse_token=os.environ.get("TOKEN"))
@@ -18,13 +18,17 @@ X_train, X_test, y_train, y_test = cnn.get_dataset()
 model = cnn.create_model(X_test=X_test, y_test=y_test)
 
 # start training
-model = cnn.train(model, X_train, y_train, n_epochs=10)
+model = cnn.train(model, X_train, y_train, n_epochs=10, save_model=True)
 
 cnn.test(model, X_test, y_test)
 
 # compile it and start the execution
 cnn.compile()
 cnn.execute()
+
+# download onnx model and test it
+onnx.download_onnx_model('model.pkl','cnn')
+onnx.test_onnx_model('cnn.onnx')
 
 loss, acc = cnn.get_score()
 print("Loss: ", loss)
