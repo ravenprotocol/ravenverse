93cba8db5f3a23c56e87186cdff032ed12de01d4
K Pranit Abhinav <impranitabhinav@gmail.com>
2022-10-31 15:35:36 +0530

    added one training script

--- Requester/create_distributed_nst.py
+++ Requester/create_distributed_nst.py
@@ -18,6 +18,140 @@ style_layers = ['Conv2D_1',
 
 
 
+import pickle as pkl
+
+import numpy as np
+import ravop as R
+
+
+from ravdl.v2 import NeuralNetwork
+from ravdl.v2.layers import Activation, Dense, BatchNormalization, Dropout, Conv2D, Flatten, MaxPooling2D
+from ravdl.v2.optimizers import Adam, RMSprop
+from ravdl.v2.loss_functions import CrossEntropy,SquareLoss
+
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+
+
+def to_categorical(x, n_col=None):
+    if not n_col:
+        n_col = np.amax(x) + 1
+    one_hot = np.zeros((x.shape[0], n_col))
+    one_hot[np.arange(x.shape[0]), x] = 1
+    return one_hot
+
+
+def get_dataset():
+
+    from keras_preprocessing import image
+    from keras.applications.vgg16 import preprocess_input
+    img_path="/Users/pranitkandarpa/Desktop/style_image.jpeg"
+    img = image.load_img(img_path, target_size=(224, 224))
+    img = image.img_to_array(img)
+    print(img)
+
+    print(np.shape(img))
+    img = np.expand_dims(img, axis=0)
+    img = preprocess_input(img)
+    from numpy import moveaxis
+    print(np.shape(img))
+    img= moveaxis(img, 3, 1)
+    # img = img.reshape((-1, 224, 224,3))
+    print("=================\n\n\n")
+    print(img,np.shape(img))
+    return img, img ,[[1]] ,[[1]]
+    pass
+
+
+
+def create_model():#n_hidden, n_features):
+    optimizer = Adam()
+    model = NeuralNetwork(optimizer=optimizer, loss='SquareLoss')
+
+
+
+
+
+    model.add(Conv2D(input_shape=(3,224,224),n_filters=64,filter_shape=(3,3),padding="same"))
+    model.add(Activation('relu'))
+    model.add(Conv2D(n_filters=64,filter_shape=(3,3),padding="same"))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_shape=(2,2),stride=2))
+
+    model.add(Conv2D(n_filters=128, filter_shape=(3,3), padding="same"))
+    model.add(Activation('relu'))
+    model.add(Conv2D(n_filters=128, filter_shape=(3,3), padding="same"))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_shape=(2,2),stride=2))
+
+    model.add(Conv2D(n_filters=256, filter_shape=(3,3), padding="same"))
+    model.add(Activation('relu'))
+    model.add(Conv2D(n_filters=256, filter_shape=(3,3), padding="same"))
+    model.add(Activation('relu'))
+    model.add(Conv2D(n_filters=256, filter_shape=(3,3), padding="same"))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_shape=(2,2),stride=2))
+
+    model.add(Conv2D(n_filters=512, filter_shape=(3,3), padding="same"))
+    model.add(Activation('relu'))
+    model.add(Conv2D(n_filters=512, filter_shape=(3,3), padding="same"))
+    model.add(Activation('relu'))
+    model.add(Conv2D(n_filters=512, filter_shape=(3,3), padding="same"))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_shape=(2,2),stride=2))
+
+    model.add(Conv2D(n_filters=512, filter_shape=(3,3), padding="same"))
+    model.add(Activation('relu'))
+    model.add(Conv2D(n_filters=512, filter_shape=(3,3), padding="same"))
+    model.add(Activation('relu'))
+    model.add(Conv2D(n_filters=512, filter_shape=(3,3), padding="same"))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_shape=(2,2),stride=2))
+
+    model.add(Flatten())
+    model.add(Dense(4096))
+    model.add(Activation('relu'))
+    model.add(Dense(4096))
+    model.add(Activation('relu'))
+    model.add(Dense(1000))
+    model.add(Activation('softmax'))
+    model.summary()
+
+    return model
+
+
+
+
+def train(model, X_train, y_train, n_epochs=5):
+    model.fit(X_train, y_train, n_epochs=n_epochs, batch_size=1, persist_weights=True) #v2
+    # model.fit(X_train, y_train, n_epochs=n_epochs, batch_size=256, save_model=True) #v1
+    # pkl.dump(model, open("cnn_model.pkl", "wb"))
+    return model
+
+def forward_p(model,X_train):
+    out=model._forward_pass(R.t(X_train),training=False,return_all_layer_output=True)
+    return out
+
+
+def test(model, X_test, y_test):
+    print('\nTesting...')
+    loss, acc = model.test_on_batch(R.t(X_test), R.t(y_test))
+    # loss.persist_op(name='cnn_test_loss')
+    # acc.persist_op(name='cnn_test_acc')
+
+def compile():
+    R.activate()
+
+
+def execute():
+    R.execute()
+    R.track_progress()
+
+
+
+
+
+
 # Initialize and create graph
 R.initialize(ravenverse_token=os.environ.get("TOKEN"))
 # R.initialize(ravenverse_token="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjY1NzgzOTgwLCJpYXQiOjE2NjMwNjI5MzMsImp0aSI6ImVhMDBiM2Q3NTE0NjRlNDZhMGYzYzg3ZTUzMmRkZjNhIiwidXNlcl9pZCI6Ijc4MjA4MjgwNDUifQ.xAY2HFgzFYASKVBubDV0fgQOfWeNQuZai6eb_vJl89M")
@@ -49,10 +183,7 @@ for _ in range(30):
     for sty in style_layers:
         style_layer_out.append(layer_vals[sty])
         layer_vals[sty].persist_op(name = sty+"_iter"+str(_))
-    # conv2d_1=layer_vals['Conv2D_1']
-    # conv2d_1.persist_op(name = "val_{}_batch_{}".format('conv1',_))
-    # Activation_10_relu=layer_vals['Activation_10_relu']
-    # Activation_10_relu.persist_op(name = "val_{}_batch_{}".format('activ_10_',_))
+
     print(layer_vals)
 
 
