6e2172f4bc9ac4295a4c054d86cc85e9dc0144cf
Anirudh Menon <senseianirudh28@gmail.com>
2023-04-20 15:34:16 +0530

    Pytorch model op and gpu script added, Provider with new participate function

--- Provider/run_distributed_provider.py
+++ Provider/run_distributed_provider.py
@@ -4,7 +4,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-from ravpy.initialize import initialize
 from ravpy.distributed.participate import participate
 import argparse
 
@@ -14,5 +13,4 @@ if __name__ == '__main__':
     parser.add_argument("--graph_id", help="Set graph id to participate", type=int)
 
     args = parser.parse_args()
-    client = initialize(os.environ.get("TOKEN"))
-    participate(graph_id=args.graph_id)
+    participate(token=os.environ.get("TOKEN"), graph_id=args.graph_id)
